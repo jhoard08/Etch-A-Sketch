55e74e756243cb4864ee80cdbc9a230acc53257b
Jake <jacob37857@gmail.com>
2022-03-27 14:59:14 -0400

    done a few touch up styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('.reset');
 
-let userInput = prompt('How many rows/columns would you like?');
-
 const makeRows = (rows, cols) => {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
@@ -10,18 +8,19 @@ const makeRows = (rows, cols) => {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
     cell.addEventListener('mouseover', () => {
-        cell.setAttribute('style', 'background-color: black;');
+      cell.setAttribute('style', 'background-color: black;');
     });
   };
 };
 
 button.addEventListener('click', () => {
-    const reset = document.querySelectorAll('.grid-item');
-    reset.forEach(paint => {
-        paint.setAttribute('style', 'background-color: #DCDCDC');
-    });
-    //userInput = prompt('How many rows/columns would you like?');
+  const reset = document.querySelectorAll('.grid-item');
+  reset.forEach(paint => {
+    paint.remove('.grid-item');
+  });
+  userInput = prompt('How many rows/columns would you like?');
+  makeRows(userInput, userInput);
 });
 
-makeRows(userInput, userInput);
+makeRows(100, 100);
 
